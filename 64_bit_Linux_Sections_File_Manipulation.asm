3212a466914c92f74e15afe1e143f525ba2deffb
Matthew-PargaManasse <114358174+Matthew-PargaManasse@users.noreply.github.com>
2026-03-12 15:31:47 -0700

    Revise file handling in 64-bit Linux assembly
    Refactor file manipulation logic and streamline system calls.

--- 64_bit_Linux_Sections_File_Manipulation.asm
+++ 64_bit_Linux_Sections_File_Manipulation.asm
@@ -1,66 +1,54 @@
 section .text
+global main
 global _start
 _start:
-.sys_write:
-        mov rdx, len
-        mov rsi, string
-        mov rdi, 1
+
+main:
+    ; open file handle
+        mov rdx, 0                      ;mode Not required for reading
+        mov rsi, 0                      ;int flags 0 = read only
+        mov rdi, filename               ;const char *filename
+        mov rax, 2
+        syscall
+
+        cmp rax, 0                      ;Check output. Fails are negative numbers
+        jl exit                         ;jump if less than
+
+        mov r8, rax
+
+    ; read file content into buffer
+        mov rdx, 6                      ;size count of lable file_content
+        mov rsi, file_content                   ;char buf lable file_content
+        mov rdi, r8                     ;fd, this is passed from open
+        mov rax, 0
+        syscall
+
+        cmp rax, 0                      ;Check output. 0 means EOF
+        jle exit                        ;Jump if less than of equal to
+
+    ; write file content to stdout
+        mov rdx, 6                      ;size of the 'file_content' variable
+        mov rsi, file_content           ;const char buf
+        mov rdi, 1                      ;file descriptor, as in STDIN, STDOUT
         mov rax, 1
         syscall
 
-        cmp rax, 0
-        jz exit
+        cmp rax, 0                      ;Check output. Fails are neg
+        jl exit                         ;Jump if less than
 
-input:
-        mov rdx, 1                                                    ;Task: Move 1 to rdx
-        mov rsi, number                                                    ;Task: Move the label 'number' to rsi
-        mov rdi, 0                                                    ;Task: Move the number associated with STDIN to rdi
-        mov rax, 0                                                    ;Task: Move the number associated with the 64 bit sys_read to rax
+    ; close file
+        mov rdi, r8                     ;File to close
+        mov rax, 3
         syscall
 
-        mov rbx, [rsi]                                                    ;Task: Move the VALUE inside the buffer (rsi) to rbx 
+    ; exit with some grace man
+    exit:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
 
-checks:
-        cmp bl, 31h                                                    ;Task: Compare the hex representation of '1' to the lower 8 bits of rbx
-        jb input                                                       ;Task: Use a jump below instruction and point it towards the 'input' label
-        cmp bl, 39h                                                    ;Task: Compare the hex representation of '9' to the lower 8 bits of rbx
-        ja input                                                       ;Task: Use a jump above instruction and point it towards the 'input' label
-        mov [number], rbx                                                    ;Task: Move the number in rbx into the LOCATION WITHIN the label 'number'
+section .data
+filename db "/home/token-user/token.txt", 0
 
-filemanipulation:
-
-.createfile:
-        mov rsi, 0644o                                                    ;Task: Using octal notation, put 644 into rsi
-        mov rdi, filename                                                    ;Task: Move the 'filename' label into rdi
-        mov rax, 85                                                    ;Task: Move the number associated with the 64 bit sys_creat to rax
-        syscall
-
-        mov [fd], rax                                                    ;Task: Move the return value from rax into the LOCATION WITHIN the label 'fd'
-
-.writefile:
-        mov rdx, 1                                                    ;Task: Move the size of the 'number' variable into rdx
-        mov rsi, number                                                    ;Task: Move the label 'number' into rsi 
-        mov rdi, [fd]                                                    ;Task: Move the VALUE within the label 'fd' into rdi
-        mov rax, 1                                                    ;Task: Move the number associated with the 64 bit sys_write to rax
-        syscall
-
-.closefile:
-        mov rdi, [fd]                                                    ;Task: Move the VALUE within the label 'fd' into rdi
-        mov rax, 3                                                    ;Task: Move the number associated with the 64 bit sys_close to rax
-        syscall
-
-
-exit:
-        xor rdi, rdi
-        mov rax, 60
-        syscall
-
-section .data                                                         ;Task: Set up the section which deals with initialized data
-        string: db "Please enter a number between 1-9: ", 0x0a
-        len:     equ $ - string
-        filename: db "Number.txt", 0x00
-
-section .bss                                                        ;Task: Set up the section which deals with uninitialized data
-        number: resb 1                                                        ;Task: Reserve one byte for a variable called 'number'
-        fd: resb 1                                                        ;Task: Reserve one byte for a variable called 'fd'
-                                                                                                                                                                                                                                                      66,1          Bot
+section .bss
+file_content resb 6
